17c48f15aac4fa22ae9d1e4b26ebf1b3ffca7538
Sijmen Brakenhoff <sijmen.brakenhoff@schiphol.nl>
2022-11-17 16:18:07 +0100

    Alsteblieft Rino

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,33 +1,26 @@
 package org.acme;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.time.Clock;
 import java.time.LocalTime;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toMap;
 
 @Path("/api")
 public class GreetingResource {
-    @Inject
-    Clock clock;
-
-    public Map<String, LocalTime> getFlights() {
-        return IntStream.range(1, 1439)
+    private static Map<String, LocalTime> getFlights() {
+        return IntStream.range(0, 1440)
                 .boxed()
-                .collect(Collectors.toMap(i -> String.format("KL%d00%d00", i / 24, i % 60),
-                        i -> LocalTime.of(i / 24, i % 60)));
+                .collect(toMap(i -> String.format("KL%02d%02d", i / 60, i % 60), i -> LocalTime.of(i / 60, i % 60)));
     }
 
     enum COLOR {
-        RED,
-        YELLOW,
-        GREEN
+        RED, YELLOW, GREEN
     }
 
     // vluchtnr + departure times
@@ -40,10 +33,10 @@ public class GreetingResource {
     public COLOR hello(@PathParam("flightNr") String flightNr, @PathParam("travelTime") Integer travelTime) {
         System.out.println("Hello Schiphol: " + flightNr + ", travelTime=" + travelTime);
         final LocalTime departureTime = flights.get(flightNr);
-        if (LocalTime.now(clock).plusMinutes(travelTime).isBefore(departureTime.minusHours(2))) {
+        if (LocalTime.now().plusMinutes(travelTime).isBefore(departureTime.minusHours(2))) {
             // Huidige tijd + reistijd < inchecktijd
             return COLOR.RED;
-        } else if (!LocalTime.now(clock).isBefore(departureTime.minusHours(2))) {
+        } else if (!LocalTime.now().isBefore(departureTime.minusHours(2))) {
             // Huidige tijd >= inchecktijd
             return COLOR.GREEN;
         }
--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -12,8 +12,6 @@ public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        // Clock clock = Clock.fixed(Instant.parse("2022-11-17T00:00:00.00Z"), ZoneId.of("UTC"));
-
         given()
                 .when().get("/api/KL2222/60")
                 .then()
